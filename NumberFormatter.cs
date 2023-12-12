26a7d34eb74eace118865fffdfb7ce3f992e722a
Kyrumin <47701566+Kyrumin@users.noreply.github.com>
2023-12-12 21:19:05 +0300

    Add files via upload

---
+++ NumberFormatter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Kyrumin
+{
+    public static class NumberFormatter
+    {
+        private static readonly string[] suffixes = {"", "K", "M", "B", "T"};
+
+        public static string FormatNumber<T>(T num) where T : struct, IComparable, IFormattable, IConvertible
+        {
+            if (num.CompareTo(Convert.ChangeType(0, typeof(T))) == 0) return "0";
+
+            double value = Convert.ToDouble(num);
+            int suffixIndex = 0;
+
+            while (suffixIndex + 1 < suffixes.Length && value >= 1000d)
+            {
+                value /= 1000d;
+                suffixIndex++;
+            }
+
+            // Возвращаем отформатированное число с соответствующим суффиксом
+            return value.ToString("#.##") + suffixes[suffixIndex];
+        }
+    }
+}
